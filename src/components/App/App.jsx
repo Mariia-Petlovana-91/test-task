bfaf156c19dfcaa7c927064a3b144a3ec75aa1b6
Mariia <90716920+Mariia-Petlovana-91@users.noreply.github.com>
2025-03-10 15:33:24 +0200

    add base setting

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,11 +1,11 @@
-import css from "../App/App.module.css";
+import css from '../App/App.module.css';
 
 function App() {
-
   return (
     <>
+      <h1>hello world</h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
