7ca874ff46081efe551adfb6fb0ef08de3363f96
Mariia <90716920+Mariia-Petlovana-91@users.noreply.github.com>
2025-03-11 12:27:49 +0200

    .

--- src/utils/validation.js
+++ src/utils/validation.js
@@ -2,12 +2,12 @@ import * as Yup from 'yup';
 
 export const schemaValidationForm = Yup.object().shape({
   name: Yup.string()
-    .min(2, 'Name must be at least 2 characters long')
-    .required('Name is required'),
+    .min(2, 'Name must be at least 2 characters long!')
+    .required('Name is required!'),
   email: Yup.string()
-    .email('Invalid email format')
-    .required('Email is required'),
+    .email('Invalid email format!')
+    .required('Email is required!'),
   phone: Yup.string()
-    .required('Phone number is required')
-    .matches(/^[+0-9]{10,15}$/, 'Invalid number phone'),
+    .required('Phone number is required!')
+    .matches(/^[+0-9]{10,15}$/, 'Invalid number phone!'),
 });
